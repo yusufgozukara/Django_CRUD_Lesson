9598267540c0671e36ecabc8b35ab21083251304
yusufgozukara <yusufgozukara11@gmail.com>
2022-08-30 18:07:04 +0300

    Read finish

--- fscohort/urls.py
+++ fscohort/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, student_list
 
 urlpatterns = [
 
-    path('', index),
+    path('', index, name='index'),
+    path('list/', student_list, name='list'),
 ]
--- fscohort/views.py
+++ fscohort/views.py
@@ -1,6 +1,24 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Student
+from .forms import StudentForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'fscohort/index.html')
+
+def student_list(request): 
+    students = Student.objects.all()
+    context = {
+        "students" : students
+    }
+    return render(request, 'fscohort/student_list.html', context)
+
+def student_add(request):
+    form = StudentForm()
+    context = {
+        "form" : form
+    }
+
+    return render(request, 'fscohort/student_add.html', context)
